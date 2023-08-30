458ea945b56cfcd7c6f8f2bab6f29317cc9cb35b
Lea_Bechard <lbechard@artfx.fr>
2023-08-30 21:46:57 +0200

    Update shottracker_webhook.py
    General clean up, more readable, not able to test the code as the project doesn't exist anymore

--- shottracker_WebHook/shottracker_webhook.py
+++ shottracker_WebHook/shottracker_webhook.py
@@ -1,29 +1,16 @@
 # IMPORT
-import requests
-from requests.exceptions import HTTPError
-from datetime import *
-from  html.parser import HTMLParser
-from discord_webhook import DiscordWebhook, DiscordEmbed
 
 import json
+import requests
+from requests.exceptions import HTTPError
+from datetime import date
+from html.parser import HTMLParser
+from discord_webhook import DiscordWebhook, DiscordEmbed
 
 DEBUG = False
 
-# CUSTOM FOR THE PFE PROJECT
+# FUNCTIONS
 
-# On ShotTracker, go in an asset or a shot, Right Click on the thumbnail and Select "Copy image adress"
-# The pfeID is in the adress : 
-#       http://tracker.artfx.fr/tracker/data/vignette/00/1234/960/image.png
-#                                                     ^^ pfeID
-
-# Dans ShotTracker, va dans un asset ou un shot, Clic Droit sur l'image et choisi "Copier le lien de l'image"
-# Le pfeID est dans l'adresse : 
-#       http://tracker.artfx.fr/tracker/data/vignette/00/1234/960/image.png
-#                                                     ^^ pfeID
-
-# DEF FUNCTION
-
-# Code de Fabien
 def getSession(login, password):
     url = "http://tracker.artfx.fr/tracker/index.php"
     rData = {"login" : login, "password" : password}
@@ -54,91 +41,75 @@ def getItemData(session, itemId):
     response = requests.get(url, params=rParm, cookies=rCookie)
     return response.json()
 
-# Parser class pour lire le HTML
-class MyHTMLParser(HTMLParser):
-    text = ""
-    def handle_data(self, data):
+class myHTMLParser(HTMLParser):
+    """Custom Parser class to read HTML"""
+    def handleData(self, data):
         self.text += data
 
-def sendSTDigest(urlWebHook, loginID, loginPW, pfeID):
-    parser = MyHTMLParser()
+def readHTML(tempText):
+    myHTMLParser().feed(tempText)
+    myHTMLParser().close()
+    tempResult = myHTMLParser().text
+    tempResult = tempResult.replace(' ', ' ').strip()
+    tempResult = tempResult.replace('`', '')
+    if len(tempResult) == 0:
+        tempResult = '// Deleted //'
+    if len(tempResult) >= 70:
+        tempResult = tempResult[0:70] + '...'
+    return tempResult
 
-    # VARIABLES
-    today = str(date.today())
-    vignette = []
-    postDiscordList = {}
-    count = 0
-
-    ### Log in ShotTracker
-    key = getSession( loginID, loginPW )
-    items = getItems( key, pfeID )
-
-    # Query the data from ShotTracker 
-    for item in items:
-        tempPost = item['name']
-        idData = item['id']
-        itemData = getItemData( key, idData )
-
-        for comment in itemData['comments']:
-            tempName = comment['firstname'].strip() +' '+ comment['lastname'].strip()
-            tempDate = comment['creationdate']
-            if comment['files']==[]:
+def getPostList(itemData, tempPost, postDiscordList, vignette, postCount):
+    for comment in itemData['comments']:
+        tempName = comment['firstname'].strip() + ' ' + comment['lastname'].strip()
+        tempDate = comment['creationdate']
+        # Select only the posts that are from today and 
+        if tempDate[0:10] == str(date.today()):
+            postCount += 1
+            if comment['files'] == []:
                 tempFile = False
             else:
                 tempFile = True
             tempText = comment['text']
-            parser = MyHTMLParser()
-            parser.feed(tempText)
-            parser.close()
-            tempResult = parser.text
-            tempResult = tempResult.replace(' ', ' ').strip()
-            tempResult = tempResult.replace('`', '')
-            if len(tempResult)==0:
-                tempResult = '// Deleted //'
-            if len(tempResult) >= 70:
-                tempResult = tempResult[0:70]+'...'
+            tempText = readHTML(tempText)
+            # append the data to the list
+        
+            if tempPost not in postDiscordList:
+                postDiscordList[tempPost] = [ [ tempDate, tempName, tempText, tempFile ] ]
+            else:
+                postDiscordList[tempPost].append( [ tempDate, tempName, tempText, tempFile ] )
+                postDiscordList[tempPost].sort()
+            postDiscordList = {k: v for k, v in sorted(postDiscordList.items())}
+            
+            if len( str(itemData['vignette']) ) != 0 and ' ' not in str(itemData['vignette']):
+                vignette = str(itemData['vignette'])
+    return postDiscordList, vignette, postCount
 
-            # Select only the posts that are from today and append the data to the list
-            if tempDate[0:10] == today:
-                if tempPost not in postDiscordList:
-                    count += 1
-                    postDiscordList[tempPost] = [ [ tempDate, tempName, tempResult, tempFile ] ]
-                else:
-                    count += 1
-                    postDiscordList[tempPost].append( [ tempDate, tempName, tempResult, tempFile ] )
-                    postDiscordList[tempPost].sort()
-                postDiscordList = {k: v for k, v in sorted(postDiscordList.items())}
-                
-                if len( str(itemData['vignette']) ) != 0 and ' ' not in str(itemData['vignette']):
-                    vignette = 'http://tracker.artfx.fr/tracker/data/vignette/'+str(pfeID)+'/'+str(idData)+ '/960/' + str(itemData['vignette'])
+def createField( postDiscordList ):
+    fieldName = []
+    fieldValue = []
+    for cle, valeur in postDiscordList.items():
+        message = ''
+        for dataList in valeur:
+            message +=  dataList[1]+ ' at '+dataList[0][-8:-3]+' : '
+            if dataList[3]:
+                message += ':link: '
+            message +=  ' `' + dataList[2] + '`\n'
+        fieldName.append( '[ ' + str(cle) + ' ] ' )
+        fieldValue.append( message )
+    return fieldName, fieldValue
 
-    # Log out from ShotTracker
-    endSession( key )
-
-    ### WebHook in Achromatic Discord
-    webhook = DiscordWebhook(
-        url=urlWebHook # default urlWebHook, change this to DEBUG to send it to my Discord Test Server instead
-        )
+def webhookMaker( postDiscordList, vignette, postCount ):
+    """Create the for the daily sum up field by field"""
+    webhook = DiscordWebhook( url=urlWebHook )
 
     embed = DiscordEmbed(
-        title=' Daily Sum Up -  '+today, 
-        description='> '+str(count)+' new message(s)', 
+        title=' Daily Sum Up -  '+str(date.today()), 
+        description='> '+str(postCount)+' new message(s)', 
         color='FF0000')
      
     # if Messages, set fields by posts 
-    if count != 0:
-        fieldName = []
-        fieldValue = []
-        for cle, valeur in postDiscordList.items():
-            message = ''
-            for dataList in valeur:
-                message +=  dataList[1]+ ' at '+dataList[0][-8:-3]+' : '
-                if dataList[3]:
-                    message += ':link: '
-                message +=  ' `' + dataList[2] + '`\n'
-            fieldName.append( '[ ' + str(cle) + ' ] ' )
-            fieldValue.append( message )
-            
+    if postCount != 0:
+        fieldName, fieldValue = createField( postDiscordList )
         embed.set_thumbnail(url=vignette)
         for id, name in enumerate(fieldName):
             # add fields to embed
@@ -149,7 +120,7 @@ def sendSTDigest(urlWebHook, loginID, loginPW, pfeID):
                 )
 
     embed.set_author(
-        name = 'ＳｈｏｔＴｒａｃｋｅｒ',
+        name = 'ShotTracker',
         url = 'http://tracker.artfx.fr',
         icon_url='http://tracker.artfx.fr/tracker/images/logo-tracker.png')
 
@@ -157,9 +128,55 @@ def sendSTDigest(urlWebHook, loginID, loginPW, pfeID):
     response = webhook.execute()
 
 
+def main(urlWebHook, loginID, loginPW, pfeID):
+
+    ### Variables
+    postDiscordList = {}
+    vignette = []
+    postCount = 0
+    ### Log in ShotTracker
+    key = getSession( loginID, loginPW )
+
+    # Query the data from ShotTracker 
+    for item in getItems( key, pfeID ):
+        idData = item['id']
+        itemData = getItemData( key, idData )
+        postDiscordList, vignette, postCount = getPostList( 
+                                                itemData, 
+                                                item['name'], 
+                                                postDiscordList, 
+                                                vignette, 
+                                                postCount )
+
+    # Log out from ShotTracker
+    endSession( key )
+    # Create a WebHook Post in Discord with the collected data
+    webhookMaker( postDiscordList, 
+        'http://tracker.artfx.fr/tracker/data/vignette/' + str(pfeID) + '/' + str(idData) + '/960/' + vignette, 
+        postCount )
+
+
+### MAIN CODE
+
 with open('./projects.json') as f:
   projects_data = json.load(f)
 
-  for project, data in projects_data['projects'].items():
-      if (DEBUG and project == "debug") or (not DEBUG and project != "debug"):
-           sendSTDigest(data["urlWebHook"], data["loginID"], data["loginPW"], data["pfeID"])
+    for project, data in projects_data['projects'].items():
+        if (DEBUG and project == "debug"):
+            main('DEBUG', data["loginID"], data["loginPW"], data["pfeID"])
+        elif (not DEBUG and project != "debug"):
+            main(data["urlWebHook"], data["loginID"], data["loginPW"], data["pfeID"])
+
+# CUSTOM FOR THE PFE PROJECT
+
+# On ShotTracker, go in an asset or a shot, 
+# Right Click on the thumbnail and Select "Copy image adress"
+# The pfeID is in the adress : 
+# http://tracker.artfx.fr/tracker/data/vignette/00/1234/960/image.png
+#                                               ^^ pfeID
+
+# Dans ShotTracker, va dans un asset ou un shot, 
+# Clic Droit sur l'image et choisi "Copier le lien de l'image"
+# Le pfeID est dans l'adresse : 
+# http://tracker.artfx.fr/tracker/data/vignette/00/1234/960/image.png
+#                                               ^^ pfeID
